234ff8cd1f380b1eb332ec93a6ce60c3b7fa9f17
Emmanuel Sackey <aximilli1212@gmail.com>
2021-10-27 23:41:39 +0000

    Add counter basics

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import {Button} from "./components/button/button";
-
+import {Counter} from  "./components/counter/Counter"
 function App() {
   return (
     <div className="App">
