bf7639b446fbfbc0d2d19828eb32627d6ee29538
Emmanuel Sackey <aximilli1212@gmail.com>
2021-10-27 19:02:30 +0000

    Add tests scaffold

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
+import './button-styles.css'
 
 export const Button = ({label}) =>{
-    return <div data-testid="button"> {label}</div>
+    return <div data-testid="button" className="button-style"> {label}</div>
 }
