77933cde74f21655a399777eda27745dd5362bac
Emmanuel Sackey <aximilli1212@gmail.com>
2021-10-28 00:21:45 +0000

    Add test for initializer in input

--- src/components/counter/__test__/Counter.test.js
+++ src/components/counter/__test__/Counter.test.js
@@ -4,7 +4,19 @@ import {render} from '@testing-library/react'
 import "@testing-library/jest-dom"
 
 test("Header renders correctly", ()=>{
-    const component = render(<Counter />); // Render Component.
-    const headerEl = component.getByTestId("header"); //Get Particular element to be tested.
+    const {getByTestId} = render(<Counter />); // Render Component.
+    const headerEl = getByTestId("header"); //Get Particular element to be tested.
     expect(headerEl.textContent).toBe("My Counter") //What we expert to see inside of it.
+})
+
+test("counter initially starts with 0", ()=>{
+     const {getByTestId} = render(<Counter />)
+     const counterEl = getByTestId("counter")
+    expect(counterEl.textContent).toBe("0")
+})
+
+test("Initial for  value in counter input to be 1", ()=>{
+    const {getByTestId} = render(<Counter />)
+    const inputEl = getByTestId("input")
+    expect(inputEl.value).toBe(1)
 })
