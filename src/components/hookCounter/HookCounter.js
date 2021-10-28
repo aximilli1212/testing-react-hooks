d29d40f3fd84a47d5bc447e730f6ce0b3e70fa3b
Emmanuel Sackey <aximilli1212@gmail.com>
2021-10-28 10:16:19 +0000

    Add test for useCounter hooks increment method

--- src/components/hookCounter/HookCounter.js
+++ src/components/hookCounter/HookCounter.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {useCounter} from "../../hooks/useCounter";
 
 export const HookCounter = ()=>{
-
+        const {count, increment, decrement}  = useCounter();
     return (
         <>
            <p>{count}</p>
