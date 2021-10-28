64b0bc31964e97fd53da37c02595a43d70a612f6
Emmanuel Sackey <aximilli1212@gmail.com>
2021-10-28 00:02:49 +0000

    run expect in test

--- src/components/counter/__test__/Counter.test.js
+++ src/components/counter/__test__/Counter.test.js
@@ -4,6 +4,7 @@ import {render} from '@testing-library/react'
 import "@testing-library/jest-dom"
 
 test("Header renders correctly", ()=>{
-    const component = render(<Counter />); // Render Component
-    const headerEl = component.getByTestId("header"); //Get Particular element to be tested
+    const component = render(<Counter />); // Render Component.
+    const headerEl = component.getByTestId("header"); //Get Particular element to be tested.
+    expect(headerEl.textContent).toBe("My Counter") //What we expert to see inside of it.
 })
